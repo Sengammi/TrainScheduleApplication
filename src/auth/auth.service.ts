9b09bce33928063bf1bb6289218ba0bda052d393
Sengammi <gfdkj54387@gmail.com>
2024-11-26 03:01:36 +0200

    Realize refresh token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from "nestjs-typegoose";
+import { UserModel } from "../user/user.model";
+import { ModelType } from "@typegoose/typegoose/lib/types";
+import { JwtService } from "@nestjs/jwt";
+import { RefreshTokenDto } from "./dto/refreshToken.dto";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
+					private readonly jwtService: JwtService) {}
+	
+	async getNewTokens ({refreshToken}: RefreshTokenDto) {
+		if (!refreshToken) {
+			throw new UnauthorizedException('Please sing in!')
+		}
+		
+		const result = await this.jwtService.verifyAsync(refreshToken)
+		
+		if (!result){
+			throw new UnauthorizedException('Invalid refresh token or expired')
+		}
+		
+		const user = await this.UserModel.findById(result._id)
+		const tokens = await this.issueTokenPair(result._id)
+		
+		return{
+			user: this.returnUserFields(user),
+			...tokens,
+		}
+	}
+	
+	async issueTokenPair(userId: string) {
+		const data = { _id: userId };
+		
+		const refreshToken = await this.jwtService.signAsync(data, {
+			expiresIn: '15d'
+		});
+		const accessToken = await this.jwtService.signAsync(data, {
+			expiresIn: '10h'
+		});
+		
+		return {
+			accessToken,
+			refreshToken,
+		}
+	}
+	
+	returnUserFields(user: UserModel) {
+		return{
+			_id: user._id,
+			username: user.username,
+			email: user.email,
+			isAdmin: user.isAdmin
+		}
+	}
+
+}
---
+++ src/auth/dto/refreshToken.dto.ts
@@ -0,0 +1,6 @@
+import {IsString} from "class-validator";
+
+export class RefreshTokenDto {
+    @IsString({message: "You did not pass refresh token of it is not a string!"})
+    refreshToken: string
+}
