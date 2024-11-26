82e0db793fd6c98ae04d7ee4e24bb515467b6f9a
Sengammi <gfdkj54387@gmail.com>
2024-11-26 02:19:48 +0200

    Create JWT token

---
+++ src/config/jwt.config.ts
@@ -0,0 +1,7 @@
+import { ConfigService } from "@nestjs/config";
+import {JwtModuleOptions} from "@nestjs/jwt";
+
+
+export const getJWTConfig = async (ConfigService: ConfigService): Promise<JwtModuleOptions> => ({
+	secret: ConfigService.get('JWT_SECRET')
+})
