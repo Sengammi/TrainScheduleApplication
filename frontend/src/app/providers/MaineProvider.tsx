3c224a4d32a580e89aafeede5f816f0195b03308
Pavlo <gfdkj54387@gmail.com>
2024-12-01 01:04:22 +0200

    Add redux toastr

--- frontend/src/app/providers/MaineProvider.tsx
+++ frontend/src/app/providers/MaineProvider.tsx
@@ -2,6 +2,7 @@ import { FC, PropsWithChildren } from 'react'
 import { HeadProvider } from "@/app/providers/HeadProvider";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Layout } from "@/app/components/layout/Layout";
+import { ReduxToast } from "@/app/providers/ReduxToast";
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -15,6 +16,7 @@ export const MaineProvider: FC<PropsWithChildren> = ({children}) => {
 	return (
 		<HeadProvider>
 			<QueryClientProvider client={queryClient}>
+				<ReduxToast/>
 				<Layout>
 					{children}
 				</Layout>
